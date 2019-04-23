8c4a2a05feb5345b6890b2be2ed7f758b1050e53
Your Name <you@example.com>
2019-04-23 11:11:31 -0700

    game header set in place and responsive

---
+++ assets/game.js
@@ -0,0 +1,36 @@
+/* Establish an array of words */
+
+var words = [
+    "cooper",
+    "laura",
+    "trees",
+    "diane",
+    "pie",
+    "coffee",
+    "bob"
+];
+
+/* randomly choose a word from the array */
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+console.log(word);
+
+/* Create an array of blanks for the chosen word. */
+
+var letterArray = [];
+for (var i = 0; i < word.length; i++) {
+    letterArray[i] = "_";
+}
+
+console.log(letterArray);
+
+/* remainingLetters is set to the length of the chosen word. This will help keep 
+track of how many letters are left to be guessed */
+
+var remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+
+    
+}
