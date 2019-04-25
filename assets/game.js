71bdadc1386f3d2bc160631894d14e87a3f6b975
SteshaMcKindle <cleodecarabas@yahoo.com>
2019-04-25 13:53:44 -0700

    random word blanks added to html with JS

--- assets/game.js
+++ assets/game.js
@@ -18,19 +18,30 @@ console.log(word);
 
 /* Create an array of blanks for the chosen word. */
 
+
 var letterArray = [];
 for (var i = 0; i < word.length; i++) {
     letterArray[i] = "_";
 }
-
 console.log(letterArray);
 
-/* remainingLetters is set to the length of the chosen word. This will help keep 
+var letterString = letterArray.join(" ");
+document.getElementById("wordInPlay").innerHTML = letterString;
+
+ /* remainingLetters is set to the length of the chosen word. This will help keep 
 track of how many letters are left to be guessed */
 
 var remainingLetters = word.length;
+console.log(remainingLetters);
 
 while (remainingLetters > 0) {
+    document.onkeyup = function(event) {
+        if {
+
+        }
+
+}
+
+}
 
     
-}
